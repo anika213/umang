bdf6c84e99cd4b3fc582eac5c282d92dac7c439d
anika213 <99388902+anika213@users.noreply.github.com>
2024-05-31 09:50:10 +0800

    Update BiddersChart.js

--- frontend/src/component/BiddersChart.js
+++ frontend/src/component/BiddersChart.js
@@ -1,18 +1,18 @@
 import React, {useEffect, useRef, useState} from 'react';
-// import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
 
 const Chart = ({CSStype,height, width, chartId,chartURL}) => {
-  // const sdk = new ChartsEmbedSDK({baseUrl: chartURL});
-  // const chartDiv = useRef(null);
-  // const [rendered, setRendered] = useState(false);
-  // const [chart] = useState(sdk.createChart({chartId: chartId, height: height, width: width, theme: "dark"})); // Create chart via SDK with relevant chartId
-  // chart.setAutoRefresh(true); 
+  const sdk = new ChartsEmbedSDK({baseUrl: chartURL});
+  const chartDiv = useRef(null);
+  const [rendered, setRendered] = useState(false);
+  const [chart] = useState(sdk.createChart({chartId: chartId, height: height, width: width, theme: "dark"})); // Create chart via SDK with relevant chartId
+  chart.setAutoRefresh(true); 
 
-  // useEffect(() => {
-  //   chart.render(chartDiv.current).then(() => setRendered(true)).catch(err => console.log("Error during Charts rendering.", err));
-  // }, [chart]);
+  useEffect(() => {
+    chart.render(chartDiv.current).then(() => setRendered(true)).catch(err => console.log("Error during Charts rendering.", err));
+  }, [chart]);
 
-  // return <div className={CSStype} ref={chartDiv}/>;
+  return <div className={CSStype} ref={chartDiv}/>;
 
 
 };
