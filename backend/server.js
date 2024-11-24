5b797d884e8362f044fef4c5eacfa69078e155e6
anika213 <sharm51155@gapps.uwcsea.edu.sg>
2024-11-23 20:20:58 -0800

    changes

--- backend/server.js
+++ backend/server.js
@@ -86,7 +86,7 @@ getimages = async (paintingnumber) => {
     // Check if painting exists and has an image field
     if (painting && painting.image) {
       // Return the URL to access the image
-      return `https://umang-818y.onrender.com/image/${painting.image}`;
+      return `http://localhost:8000/image/${painting.image}`;
     } else {
       console.log(`Painting with number ${paintingnumber} not found or doesn't have an image.`);
       return null;
@@ -350,14 +350,14 @@ app.put("/allbids/placebid", async (req, res) => {
 
     if (bidvalue > data[0].highestBid.bidvalue) {
         userLastBidTime[name] = currentTime;
-        axios.put('https://umang-818y.onrender.com/allbids/bidplaced',  
+        axios.put('http://localhost:8000/allbids/bidplaced',  
         { name, bidvalue, paintingnumber,highestBidderEmail })  
         .then((data) => {
             console.log("bidplaced"); 
             res.status(200).send(highestBidderEmail);
         })
 
-        axios.put('https://umang-818y.onrender.com/allbids/mybids',  
+        axios.put('http://localhost:8000/allbids/mybids',  
         { name, bidvalue, paintingnumber })
         .then((data) => {
             console.log("mybids")
@@ -633,7 +633,7 @@ app.get('/admin/endbidding', async (req, res) => {
       paintingsDimensions[paintingName] = paintingInfo.dimensions || 'N/A';
       paintingsThemes[paintingName] = paintingInfo.theme || 'N/A';
       paintingsHighestBids[paintingName] = paintingInfo.highestBid ? paintingInfo.highestBid.bidvalue : 'N/A';
-      imageUrls[paintingName] = `https://umang-818y.onrender.com/image?paintingnumber=${paintingName}`;
+      imageUrls[paintingName] = `http://localhost:8000/image?paintingnumber=${paintingName}`;
     });
     console.log(imageUrls)  
     res.status(200).json({
